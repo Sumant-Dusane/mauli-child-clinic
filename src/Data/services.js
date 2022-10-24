ec963b2da6ac4e8115b9fb5120237f5d8915166f
Sumant-Testbook <sumant.dusane@testbook.com>
2022-10-24 13:07:58 +0530

    changes

--- src/Data/services.js
+++ src/Data/services.js
@@ -31,7 +31,7 @@ export const ServicesData = [
     },
     {
         image: "https://thumbs.dreamstime.com/b/d-doctor-arms-crossed-illustration-isolated-white-background-d-doctor-arms-crossed-101335551.jpg",
-        title: "Medicare",
+        title: "Sumant",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tempore suscipit maxime voluptatibus velit modi, distinctio quos earum. Culpa, vitae!",
         buttontext: "Learn More"
     },
