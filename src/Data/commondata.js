eb162f5bb13ac25a054145ae4a902af316b87af2
Sumant-Testbook <sumant.dusane@testbook.com>
2022-10-25 23:00:12 +0530

    changes

--- src/Data/commondata.js
+++ src/Data/commondata.js
@@ -1,6 +1,6 @@
 export const CommonData = [
     {
-        contact: "+91 9991119910",
+        contact: "9991119910",
         address: "Mauli children hospital & Aesthetic Dental clinic, Shop no 30/31, 1st floor, Amarante Chs, sector 9E , near D mart, kalamboli Raigad",
         logo: "https://cdn-icons-png.flaticon.com/512/30/30048.png",
         logoWhite: " ",
